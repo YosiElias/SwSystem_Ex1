814a13da0ea49f5bbc98dd8999bc3e7827d24c47
YosiElias <yosielias1313@gmail.com>
2021-11-03 11:46:37 +0200

    fix_main.1

--- main.c
+++ main.c
@@ -24,19 +24,20 @@ int main()
     printf("\nThe Palindromes are:");
     for (int i=min; i<=max; i++){
         if (isPalindrome(i))
-            printf(" %d ", i);
+            printf(" %d", i);
     }
 
     printf("\nThe Prime numbers are:");
     for (int i=min; i<=max; i++){
         if (isPrime(i))
-            printf(" %d ", i);
+            printf(" %d", i);
     }
 
     printf("\nThe Strong numbers are:");
     for (int i=min; i<=max; i++){
         if (isStrong(i))
-            printf(" %d ", i);
+            printf(" %d", i);
     }
+    printf("\n");
 
 }
